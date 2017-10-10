67eedbb9119d2f0be1db7fa6ee1a6b339ce55093
Arnaud Lefèvre <Arnaud@imac-de-mafalda>
2017-10-10 14:20:42 +0200

    Initial Commit

---
+++ ON Bosse/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  ON Bosse
+//
+//  Created by Arnaud Miguet on 10.10.17.
+//  Copyright © 2017 Arnaud Miguet. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
