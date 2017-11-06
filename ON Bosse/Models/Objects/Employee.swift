d9e9e336afdaa1ada8f5e15f83b20c891654fce4
Arnaud Miguet <Arnaud@imac-de-mafalda>
2017-11-06 12:26:51 +0100

    Setup Global Architecture

--- ON Bosse/Models/Objects/Employee.swift
+++ ON Bosse/Models/Objects/Employee.swift
@@ -11,10 +11,12 @@ import Cocoa
 class Employee: Person {
     var hours_balance: Double
     var is_manager: Bool
+    var salary: Double
     
-    init(name: String, surname: String, hours_balance: Double = 0, nickname: String = "", id: Int = 0, is_manager: Bool = false) {
+    init(name: String, surname: String, hours_balance: Double = 0, nickname: String = "", id: Int = 0, is_manager: Bool = false, salary: Double) {
         self.hours_balance = hours_balance
         self.is_manager = is_manager
+        self.salary = salary
         super.init(name: name, surname: surname, nickname: nickname, id: id)
     }
 }
